a1cd15c24b721148d63aee1e6ee0f4380e67ad77
Pankaj Nagmode <nagmodepankaj2032@gmail.com>
2025-08-07 21:43:09 +0530

    Uploaded files
    index , style ,script file and some important images

---
+++ script.js
@@ -0,0 +1,18 @@
+function ScrollHome() {
+  window.scrollTo(0, 0);
+}
+function ScrollAbout() {
+  window.scrollTo(0, 400);
+}
+function ScrollSkills() {
+  window.scrollTo(0, 850);
+}
+function ScrollProjects() {
+  window.scrollTo(0, 1500);
+}
+function ScrollContact() {
+  window.scrollTo(0, 2350);
+}
+function scrollContactForm() {
+  window.scrollTo(0, 2650);
+}
